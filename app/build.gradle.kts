7502be5eda93721c872da452aea7afeaf0637bd2
sean <sean.looong@outlook.com>
2025-12-06 22:29:54 +0800

    补全room的依赖文件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -73,6 +74,7 @@ dependencies {
     implementation(libs.androidx.camera.view)
     implementation(libs.room.runtime)
     implementation(libs.androidx.room.ktx)
+    kapt(libs.room.compiler)
     implementation(libs.androidx.room.paging)
     implementation(libs.androidx.adapters)
     testImplementation(libs.junit)
