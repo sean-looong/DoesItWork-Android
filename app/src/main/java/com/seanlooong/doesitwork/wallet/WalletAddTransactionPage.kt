7f5ef979536ec75ec849ddf5197d648615d25e0d
sean <sean.looong@outlook.com>
2025-12-08 23:47:35 +0800

    添加账单输入小键盘

--- app/src/main/java/com/seanlooong/doesitwork/wallet/WalletAddTransactionPage.kt
+++ app/src/main/java/com/seanlooong/doesitwork/wallet/WalletAddTransactionPage.kt
@@ -3,16 +3,30 @@ package com.seanlooong.doesitwork.wallet
 import PermissionRequest
 import android.Manifest
 import android.annotation.SuppressLint
+
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.seanlooong.exerciseandroid.ui.widgets.SmallTopAppBar
 
@@ -44,6 +58,75 @@ fun WalletAddTransactionPage(
             )
         }
     ) {
+        Column() {
+            WalletCategoriesSelector(
+                modifier = modifier
+                    .fillMaxHeight()
+                    .weight(1f))
+            WalletKeyBoard()
+        }
+    }
+}
+
+@Composable
+fun WalletCategoriesSelector(
+    modifier: Modifier = Modifier
+) {
+    FlowRow (
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
 
     }
+}
+
+@Composable
+fun WalletKeyBoard(
+    modifier: Modifier = Modifier
+) {
+    val keys = listOf<String>(
+        "7", "8", "9", "日期",
+        "4", "5", "6", "+",
+        "1", "2", "3", "-",
+        ".", "0", "后退", "完成")
+    Column() {
+        FlowRow (
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            maxItemsInEachRow = 4
+        ) {
+            repeat(keys.size) {
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(4.dp),
+                    contentPadding = PaddingValues(horizontal = 4.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(34.dp)
+                        .weight(1f)) {
+                    Text(text = keys[it],
+                        fontSize = 14.sp,
+                        maxLines = 1,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WalletCategoriesSelectorPreview() {
+    WalletCategoriesSelector()
+}
+
+@Preview
+@Composable
+fun WalletKeyBoardPreview() {
+    WalletKeyBoard()
 }
