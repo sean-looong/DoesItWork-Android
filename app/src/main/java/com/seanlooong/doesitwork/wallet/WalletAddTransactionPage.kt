558985e056a834bd54d7b2ba465cd6d3790bc359
sean <sean.looong@outlook.com>
2025-12-21 01:43:59 +0800

    对键盘中的部分案件进行处理，完成数字输入，后退，加减法操作

--- app/src/main/java/com/seanlooong/doesitwork/wallet/WalletAddTransactionPage.kt
+++ app/src/main/java/com/seanlooong/doesitwork/wallet/WalletAddTransactionPage.kt
@@ -4,12 +4,14 @@ import PermissionRequest
 import android.Manifest
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,11 +19,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -39,13 +45,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -226,23 +236,225 @@ fun WalletCategoriesExpenseSelector(
 fun WalletKeyBoard(
     modifier: Modifier = Modifier
 ) {
+    var amount by remember { mutableStateOf("0.00") }
+    var num1 by remember { mutableStateOf("0.00") }
+    var sign by remember { mutableStateOf("") }
+    var num2 by remember { mutableStateOf("0.00") }
+    var description by remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     val keys = listOf<String>(
         "7", "8", "9", "日期",
         "4", "5", "6", "+",
         "1", "2", "3", "-",
         ".", "0", "后退", "完成")
-    Column() {
+
+    /**
+     * 处理点击日期操作
+     */
+    fun onClickCalender() {
+
+    }
+
+    /**
+     * 将num1 sign num2进行拼接
+     */
+    fun settAmount() {
+        if (num2 != "0.00") {
+            amount = num1 + sign + num2
+        } else if (sign != "") {
+            amount = num1 + sign
+        } else {
+            amount = num1
+        }
+    }
+
+    /**
+     * 对数字进行拼接
+     */
+    fun contactNum(num: String, input: String): String {
+        if (num == "0.00") {
+            return if (input == ".") "0." else input
+        } else if (num.contains(".")) {
+            return if (input == ".") num else num + input
+        } else {
+            return num + input
+        }
+    }
+
+    /**
+     * 对数值进行计算
+     */
+    fun calculateNumbers(): String {
+        val double1 = num1.toDoubleOrNull() ?: 0.00
+        val double2 = num2.toDoubleOrNull() ?: 0.00
+
+        if (sign == "+") {
+            return (double1 + double2).toCleanDecimalString()
+        } else if (sign == "-") {
+            return (double1 - double2).toCleanDecimalString()
+        } else {
+            return num1
+        }
+    }
+
+    /**
+     * 点击后退按钮
+     */
+    fun onClickBack() {
+        if (num2 != "0.00") {
+            num2 = num2.dropLast(1)
+            if (num2.isEmpty()) {
+                num2 = "0.00"
+            }
+        } else if (sign != "") {
+            sign = ""
+        } else if (num1 != "0.00") {
+            num1 = num1.dropLast(1)
+            if (num1.isEmpty()) {
+                num1 = "0.00"
+            }
+        }
+
+        settAmount()
+    }
+
+    /**
+     * 处理点击完成按钮
+     */
+    fun onClickComplete() {
+        num1 = calculateNumbers()
+        sign = ""
+        num2 = "0.00"
+        settAmount()
+
+        if (num1.startsWith("-")) {
+            // TODO 提示用户
+        }
+    }
+
+    /**
+     * 处理点击符号操作
+     */
+    fun onClickSing(input: String) {
+        if (num2 != "0.00") {
+            num1 = calculateNumbers()
+            num2 = "0.00"
+        }
+
+        sign = input
+
+        settAmount()
+    }
+
+    fun onClickNum(input: String) {
+        if (sign != "") {
+            num2 = contactNum(num2, input)
+        } else {
+            num1 = contactNum(num1, input)
+        }
+
+        settAmount()
+    }
+
+
+    /**
+     * 处理点击事件
+     */
+    fun onClickButton(text: String) {
+        when(text) {
+            "日期" -> onClickCalender()
+            "后退" -> onClickBack()
+            "完成" -> onClickComplete()
+            "+", "-" -> onClickSing(text)
+            else -> onClickNum(text)
+        }
+    }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        // 显示输入的金额大小
+        Text("CN ${amount}",
+            modifier = Modifier
+                .border(
+                    width = 1.dp,
+                    shape = RoundedCornerShape(8.dp),
+                    color = MaterialTheme.colorScheme.primaryContainer
+                )
+                .fillMaxWidth()
+                .height(34.dp)
+                .wrapContentHeight(Alignment.CenterVertically)
+                .padding(start = 10.dp, end = 10.dp),
+            color = MaterialTheme.colorScheme.primaryContainer,
+            fontSize = 16.sp,
+            textAlign = TextAlign.End)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        BasicTextField(
+            value = description,
+            onValueChange = { description = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(34.dp)
+                .border(
+                    width = 1.dp,
+                    shape = RoundedCornerShape(8.dp),
+                    color = MaterialTheme.colorScheme.primaryContainer
+                ),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done // 搜索按钮
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    // 执行搜索逻辑
+                    keyboardController?.hide()
+                }
+            ),
+            textStyle = TextStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal
+            ),
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(34.dp),
+                    contentAlignment = Alignment.CenterStart // 左对齐且垂直居中
+                ) {
+                    if (description.isEmpty()) {
+                        Text(
+                            text = "请输入",
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colorScheme.outline,
+                            modifier = Modifier.wrapContentHeight(Alignment.CenterVertically).padding(start = 10.dp, end = 10.dp)
+                        )
+                    }
+                    // 关键：给 innerTextField 添加相同的 padding, 让提示文字和输入文字对齐
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(34.dp)
+                            .padding(horizontal = 10.dp), // 相同的水平padding
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        innerTextField()
+                    }
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         FlowRow (
             modifier = modifier
-                .fillMaxWidth()
-                .padding(16.dp),
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
             maxItemsInEachRow = 4
         ) {
             repeat(keys.size) {
                 Button(
-                    onClick = {},
+                    onClick = { onClickButton(keys[it]) },
                     shape = RoundedCornerShape(4.dp),
                     contentPadding = PaddingValues(horizontal = 4.dp),
                     modifier = Modifier
@@ -257,6 +469,20 @@ fun WalletKeyBoard(
                 }
             }
         }
+    }
+}
+
+fun Double.toCleanDecimalString(): String {
+    // 使用 BigDecimal 处理精度问题
+    val bigDecimal = this.toBigDecimal()
+
+    return if (bigDecimal.scale() <= 0 ||
+        bigDecimal.stripTrailingZeros().scale() <= 0) {
+        // 没有小数部分或小数部分全是0
+        bigDecimal.toLong().toString()
+    } else {
+        // 去掉末尾的0
+        bigDecimal.stripTrailingZeros().toPlainString()
     }
 }
 
